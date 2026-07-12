1180918d4b57239787cb75f6af26d9822fad5924
apexlions16 <hcywashere@gmail.com>
2026-07-12 15:16:07 +0300

    Bozuk piksel yazı tipini geri al ve okunaklı yazı tipini ekle

--- src/PixelFont.h
+++ src/PixelFont.h
@@ -1,31 +1,89 @@
 #pragma once
 
 #include <algorithm>
+#include <cctype>
 #include <stdint.h>
 #include <string>
 
 namespace psforcer {
 
-static const uint64_t kPixelHarfler[95] = {
-    0x000000000ULL,0x39CE639C0ULL,0x7BDE00000ULL,0x01FFFFFC0ULL,0x3BDEF79CCULL,
-    0x639FFFDE0ULL,0x3BDFFFFE0ULL,0x318C00000ULL,0x39CC631C6ULL,0x31CE739CCULL,
-    0x33DEF3000ULL,0x019FFB180ULL,0x000E73000ULL,0x01CE00000ULL,0x000E73800ULL,
-    0x18CE73318ULL,0x3BDFFFFC0ULL,0x7BCE73BE0ULL,0x7BCE77BC0ULL,0x3BCE7FFC0ULL,
-    0x39DEFFCC0ULL,0x7BDE37BC0ULL,0x3BDEFFFC0ULL,0x7BCE73980ULL,0x3BDEFFFC0ULL,
-    0x3BDFFFFC0ULL,0x01CE739C0ULL,0x01CE73980ULL,0x01FEFBC00ULL,0x03FFF8000ULL,
-    0x03DFFF800ULL,0x3BCE63180ULL,0x3BFFFFFEFULL,0x39DEF7FE0ULL,0x7BFFFFFE0ULL,
-    0x3BD8C73C0ULL,0x7BFFFFFC0ULL,0x7FFEF63E0ULL,0x7FFEF6300ULL,0x3BD8FFFE0ULL,
-    0x7FFFFFFE0ULL,0x7BCE73BC0ULL,0x39C637BC0ULL,0x7FFEF7FE0ULL,0x6318C63E0ULL,
-    0x7FFFFFFE0ULL,0x7FFFFFFE0ULL,0x3BFFFFFC0ULL,0x7BFFF6300ULL,0x3BFFFFFC6ULL,
-    0x7BFFF7FE0ULL,0x3BDE7FFC0ULL,0x7FEE739C0ULL,0x7FFFFFFC0ULL,0x7FFEF79C0ULL,
-    0x6FFFFFFE0ULL,0x7FEEF7BE0ULL,0x7FFE739C0ULL,0x7FEEF73E0ULL,0x398C631CEULL,
-    0x630C738C6ULL,0x39CE739CEULL,0x3BFF00000ULL,0x0000FFC00ULL,0x218000000ULL,
-    0x03DFFFFE0ULL,0x631EFFFFEULL,0x03DEF79C0ULL,0x18DEF7BDEULL,0x03DFFFDC0ULL,
-    0x39DE6318CULL,0x3FFFFFFCEULL,0x631EF7BDEULL,0x39DE73BFFULL,0x3BCE739DEULL,
-    0x631FF7BFFULL,0x738C631EFULL,0x03FFFFFE0ULL,0x03DEF7BC0ULL,0x03DFFFDC0ULL,
-    0x7BFFFFB18ULL,0x7BDEF78C6ULL,0x01EF63180ULL,0x03DEF79C0ULL,0x33DE631C0ULL,
-    0x03DEF7BC0ULL,0x03FEF39C0ULL,0x03FFFFFC0ULL,0x03DEF7FE0ULL,0x7FFE73BDCULL,
-    0x03CEF7BC0ULL,0x39DEF39CEULL,0x318C6318CULL,0x71CE73B9CULL,0x001F70000ULL
+struct PixelHarf {
+    char karakter;
+    uint8_t satirlar[7];
+};
+
+// Temiz ve sabit 5x7 yazı takımı. Önceki sıkıştırılmış bit tablosu bazı
+// harfleri birbirine karıştırdığı için her satır açık biçimde tanımlanır.
+static const PixelHarf kPixelHarfler[] = {
+    {' ', {0, 0, 0, 0, 0, 0, 0}},
+    {'!', {4, 4, 4, 4, 4, 0, 4}},
+    {'"', {10, 10, 10, 0, 0, 0, 0}},
+    {'#', {10, 31, 10, 10, 31, 10, 0}},
+    {'$', {4, 15, 20, 14, 5, 30, 4}},
+    {'%', {25, 25, 2, 4, 8, 19, 19}},
+    {'&', {12, 18, 20, 8, 21, 18, 13}},
+    {'\'', {4, 4, 8, 0, 0, 0, 0}},
+    {'(', {2, 4, 8, 8, 8, 4, 2}},
+    {')', {8, 4, 2, 2, 2, 4, 8}},
+    {'*', {0, 21, 14, 31, 14, 21, 0}},
+    {'+', {0, 4, 4, 31, 4, 4, 0}},
+    {',', {0, 0, 0, 0, 4, 4, 8}},
+    {'-', {0, 0, 0, 31, 0, 0, 0}},
+    {'.', {0, 0, 0, 0, 0, 12, 12}},
+    {'/', {1, 2, 4, 8, 16, 0, 0}},
+    {'0', {14, 17, 19, 21, 25, 17, 14}},
+    {'1', {4, 12, 4, 4, 4, 4, 14}},
+    {'2', {14, 17, 1, 2, 4, 8, 31}},
+    {'3', {30, 1, 1, 14, 1, 1, 30}},
+    {'4', {2, 6, 10, 18, 31, 2, 2}},
+    {'5', {31, 16, 16, 30, 1, 1, 30}},
+    {'6', {14, 16, 16, 30, 17, 17, 14}},
+    {'7', {31, 1, 2, 4, 8, 8, 8}},
+    {'8', {14, 17, 17, 14, 17, 17, 14}},
+    {'9', {14, 17, 17, 15, 1, 1, 14}},
+    {':', {0, 12, 12, 0, 12, 12, 0}},
+    {';', {0, 12, 12, 0, 4, 4, 8}},
+    {'<', {2, 4, 8, 16, 8, 4, 2}},
+    {'=', {0, 31, 0, 31, 0, 0, 0}},
+    {'>', {8, 4, 2, 1, 2, 4, 8}},
+    {'?', {14, 17, 1, 2, 4, 0, 4}},
+    {'@', {14, 17, 23, 21, 23, 16, 14}},
+    {'A', {14, 17, 17, 31, 17, 17, 17}},
+    {'B', {30, 17, 17, 30, 17, 17, 30}},
+    {'C', {14, 17, 16, 16, 16, 17, 14}},
+    {'D', {30, 17, 17, 17, 17, 17, 30}},
+    {'E', {31, 16, 16, 30, 16, 16, 31}},
+    {'F', {31, 16, 16, 30, 16, 16, 16}},
+    {'G', {14, 17, 16, 23, 17, 17, 15}},
+    {'H', {17, 17, 17, 31, 17, 17, 17}},
+    {'I', {31, 4, 4, 4, 4, 4, 31}},
+    {'J', {7, 2, 2, 2, 18, 18, 12}},
+    {'K', {17, 18, 20, 24, 20, 18, 17}},
+    {'L', {16, 16, 16, 16, 16, 16, 31}},
+    {'M', {17, 27, 21, 21, 17, 17, 17}},
+    {'N', {17, 25, 21, 19, 17, 17, 17}},
+    {'O', {14, 17, 17, 17, 17, 17, 14}},
+    {'P', {30, 17, 17, 30, 16, 16, 16}},
+    {'Q', {14, 17, 17, 17, 21, 18, 13}},
+    {'R', {30, 17, 17, 30, 20, 18, 17}},
+    {'S', {15, 16, 16, 14, 1, 1, 30}},
+    {'T', {31, 4, 4, 4, 4, 4, 4}},
+    {'U', {17, 17, 17, 17, 17, 17, 14}},
+    {'V', {17, 17, 17, 17, 17, 10, 4}},
+    {'W', {17, 17, 17, 21, 21, 21, 10}},
+    {'X', {17, 17, 10, 4, 10, 17, 17}},
+    {'Y', {17, 17, 10, 4, 4, 4, 4}},
+    {'Z', {31, 1, 2, 4, 8, 16, 31}},
+    {'[', {14, 8, 8, 8, 8, 8, 14}},
+    {'\\', {16, 8, 4, 2, 1, 0, 0}},
+    {']', {14, 2, 2, 2, 2, 2, 14}},
+    {'^', {4, 10, 17, 0, 0, 0, 0}},
+    {'_', {0, 0, 0, 0, 0, 0, 31}},
+    {'`', {8, 4, 2, 0, 0, 0, 0}},
+    {'{', {2, 4, 4, 8, 4, 4, 2}},
+    {'|', {4, 4, 4, 4, 4, 4, 4}},
+    {'}', {8, 4, 4, 2, 4, 4, 8}},
+    {'~', {0, 0, 9, 22, 0, 0, 0}}
 };
 
 struct YaziOlculeri {
@@ -37,26 +95,46 @@ struct YaziOlculeri {
 inline YaziOlculeri yaziOlculeri(int yukseklik) {
     YaziOlculeri sonuc;
     sonuc.dikey = std::max(1, yukseklik / 7);
-    sonuc.yatay = std::max(1, (sonuc.dikey + 1) / 2);
-    sonuc.ilerleme = 5 * sonuc.yatay + std::max(1, sonuc.yatay);
+    sonuc.yatay = std::max(1, (sonuc.dikey * 2 + 2) / 3);
+    sonuc.ilerleme = 5 * sonuc.yatay + sonuc.yatay;
     return sonuc;
+}
+
+inline char buyukAscii(char karakter) {
+    if (karakter >= 'a' && karakter <= 'z') return static_cast<char>(karakter - 'a' + 'A');
+    return karakter;
+}
+
+inline const uint8_t* pixelHarf(char karakter) {
+    karakter = buyukAscii(karakter);
+    const size_t sayi = sizeof(kPixelHarfler) / sizeof(kPixelHarfler[0]);
+    for (size_t i = 0; i < sayi; ++i) {
+        if (kPixelHarfler[i].karakter == karakter) return kPixelHarfler[i].satirlar;
+    }
+    return pixelHarf('?');
 }
 
 inline std::string turkceyiPikselYaziyaCevir(const std::string& deger) {
     std::string sonuc;
     for (size_t i = 0; i < deger.size(); ++i) {
         const unsigned char a = static_cast<unsigned char>(deger[i]);
-        if (a >= 32 && a <= 126) { sonuc.push_back(static_cast<char>(a)); continue; }
+        if (a >= 32 && a <= 126) {
+            sonuc.push_back(buyukAscii(static_cast<char>(a)));
+            continue;
+        }
         if (i + 1 >= deger.size()) continue;
         const unsigned char b = static_cast<unsigned char>(deger[i + 1]);
         char harf = 0;
-        if (a == 0xC3 && (b == 0x87 || b == 0xA7)) harf = b == 0x87 ? 'C' : 'c';
-        else if (a == 0xC4 && (b == 0x9E || b == 0x9F)) harf = b == 0x9E ? 'G' : 'g';
-        else if (a == 0xC4 && (b == 0xB0 || b == 0xB1)) harf = b == 0xB0 ? 'I' : 'i';
-        else if (a == 0xC3 && (b == 0x96 || b == 0xB6)) harf = b == 0x96 ? 'O' : 'o';
-        else if (a == 0xC5 && (b == 0x9E || b == 0x9F)) harf = b == 0x9E ? 'S' : 's';
-        else if (a == 0xC3 && (b == 0x9C || b == 0xBC)) harf = b == 0x9C ? 'U' : 'u';
-        if (harf) { sonuc.push_back(harf); ++i; }
+        if (a == 0xC3 && (b == 0x87 || b == 0xA7)) harf = 'C';
+        else if (a == 0xC4 && (b == 0x9E || b == 0x9F)) harf = 'G';
+        else if (a == 0xC4 && (b == 0xB0 || b == 0xB1)) harf = 'I';
+        else if (a == 0xC3 && (b == 0x96 || b == 0xB6)) harf = 'O';
+        else if (a == 0xC5 && (b == 0x9E || b == 0x9F)) harf = 'S';
+        else if (a == 0xC3 && (b == 0x9C || b == 0xBC)) harf = 'U';
+        if (harf) {
+            sonuc.push_back(harf);
+            ++i;
+        }
     }
     return sonuc;
 }
